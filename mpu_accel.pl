b090a5ac8cf2269c7600f3d17b2046ddff8b6851
Kristian Nielsen <knielsen@knielsen-hq.org>
2015-08-01 14:22:52 +0200

    Also compute stddev of accelerations, for balancing measurements.

--- mpu_accel.pl
+++ mpu_accel.pl
@@ -6,6 +6,12 @@ use warnings;
 my $s;
 my $i = 0;
 
+my ($mx, $my, $mz) = 0;
+my ($sx, $sy, $sz) = 0;
+my $n = 0;
+
+my $d = [ ];
+
 while (1) {
   sysread(STDIN, $s, 6) or last;
   my $xi = (ord(substr($s, 0, 1)) << 8) | ord(substr($s, 1, 1));
@@ -19,4 +25,28 @@ while (1) {
   my $z = $zi * (2/32768);
   print $i, "\t", $x, "\t", $y, "\t", $z, "\n";
   $i += 0.001;
+
+  $mx += $x;
+  $my += $y;
+  $mz += $z;
+  ++$n;
+  push @$d, [$x, $y, $z];
 }
+
+$mx /= $n;
+$my /= $n;
+$mz /= $n;
+print STDERR "Mean:\t", $mx, "\t", $my, "\t", $mz, "\n";
+
+for (@$d) {
+  my $tmp = $_->[0] - $mx;
+  $sx += $tmp*$tmp;
+  $tmp = $_->[1] - $my;
+  $sy += $tmp*$tmp;
+  $tmp = $_->[2] - $mz;
+  $sz += $tmp*$tmp;
+}
+$sx /= ($n-1);
+$sy /= ($n-1);
+$sz /= ($n-1);
+print STDERR "Stddev:\t", sqrt($sx), "\t", sqrt($sy), "\t", sqrt($sz), "\n";
